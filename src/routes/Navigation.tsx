cfc55258e22d4bbc7fd7d9c2e185067d338b48b0
Nelson Rosales <56582884+nelson2411@users.noreply.github.com>
2025-03-15 23:31:29 +0100

    lazy and suspense added

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react"
 import { BrowserRouter } from "react-router-dom"
 import { Routes, Route, NavLink, Navigate } from "react-router-dom"
 import logo from "../logo.svg"
@@ -5,37 +6,39 @@ import { routes } from "../routes/routes"
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          <NavLink to="/">
-            <img src={logo} alt="logo" />
-          </NavLink>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <NavLink to="/">
+              <img src={logo} alt="logo" />
+            </NavLink>
 
-          <ul>
+            <ul>
+              {routes.map((route, index) => (
+                <li key={index}>
+                  <NavLink
+                    to={route.path}
+                    className={({ isActive }) => (isActive ? "nav-active" : "")}
+                  >
+                    {route.name}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Routes>
             {routes.map((route, index) => (
-              <li key={index}>
-                <NavLink
-                  to={route.path}
-                  className={({ isActive }) => (isActive ? "nav-active" : "")}
-                >
-                  {route.name}
-                </NavLink>
-              </li>
+              <Route
+                key={index}
+                path={route.path}
+                element={<route.Component />}
+              />
             ))}
-          </ul>
-        </nav>
-        <Routes>
-          {routes.map((route, index) => (
-            <Route
-              key={index}
-              path={route.path}
-              element={<route.Component />}
-            />
-          ))}
-          <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+            <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   )
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,29 +1,51 @@
+import { lazy, LazyExoticComponent } from "react"
 import { LazyPage01, LazyPage02, LazyPage03 } from "../01-lazyload/pages"
 
 interface Route {
   to: string
   path: string
-  Component: React.FC // React.Fc is a type for functional components
+  Component: React.ComponentType<any> | LazyExoticComponent<any>
   name: string
 }
+
+// Implementing lazy loading
+
+const Lazy01 = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "LazyPage01" */ "../01-lazyload/pages/LazyPage01"
+    )
+)
+const Lazy02 = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "LazyPage02" */ "../01-lazyload/pages/LazyPage02"
+    )
+)
+const Lazy03 = lazy(
+  () =>
+    import(
+      /* webpackChunkName: "LazyPage03" */ "../01-lazyload/pages/LazyPage03"
+    )
+)
 
 export const routes = [
   {
     to: "/lazy01",
     path: "lazy01",
-    Component: LazyPage01,
+    Component: Lazy01,
     name: "Lazy-01",
   },
   {
     to: "/lazy02",
     path: "lazy02",
-    Component: LazyPage02,
+    Component: Lazy02,
     name: "Lazy-02",
   },
   {
     to: "/lazy03",
     path: "lazy03",
-    Component: LazyPage03,
+    Component: Lazy03,
     name: "Lazy-03",
   },
 ] as Route[] // This is useful for TypeScript to know the type of the array
